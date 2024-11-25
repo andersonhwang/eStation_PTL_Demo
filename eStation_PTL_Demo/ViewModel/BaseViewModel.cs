eae9bfeb8a93ac702e2a5a48385462e8b61dfd85
Huang Hai Peng <47938030+andersonhwang@users.noreply.github.com>
2024-11-25 08:35:20 +0800

    Init project.

---
+++ eStation_PTL_Demo/ViewModel/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStation_PTL_Demo.ViewModel
+{
+    internal class BaseViewModel
+    {
+    }
+}
